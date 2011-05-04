a60add04c1842a7eb654ada44e7f65609fc558d2
guardo823 <guardo823@gmail.com>
2011-05-04 15:57:46 -0700

    agregue todos los metodos de area

--- src/biblioteca/database2/beans/Area.java
+++ src/biblioteca/database2/beans/Area.java
@@ -4,10 +4,52 @@
  */
 package biblioteca.database2.beans;
 
+package beans;
+
 /**
  *
- * @author alejandro
+ * @author Eduardo
  */
 public class Area {
-    
+
+    String idArea;
+    String nombre;
+    String descripcion;
+    String idSubarea;
+
+    public Area(){}
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setID(String id) {
+        this.idArea = id;
+    }
+
+    public void setIDSubarea(String idSubarea) {
+        this.idSubarea = idSubarea;
+    }
+
+   public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getID() {
+        return idArea;
+    }
+
+    public String getIDSubarea() {
+        return idSubarea;
+    }
+
+      public String getDescripcion() {
+        return descripcion;
+    }
+
+
 }
