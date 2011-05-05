db7a8a914ab427281fb5024fd278d9e9e6c26145
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-04 23:46:18 -0500

    ControladorReportesEstadisticas marcada como Deprecated.

--- src/biblioteca/database2/controladores/ControladorReportesEstadisticas.java
+++ src/biblioteca/database2/controladores/ControladorReportesEstadisticas.java
@@ -8,6 +8,14 @@ package biblioteca.database2.controladores;
  *
  * @author alejandro
  */
+
+/*
+ * Leyendo el diagrama de clases de los reportes, parece que no hay mucho cambio entre
+ * el controlador y la clase DAO, tienen los mismos metodos con entradas similares y
+ * salidas similares, solo con nombres diferentes, creo que esta clase debe ser eliminada
+ * y usar directamente el DAO.
+ */
+@Deprecated
 public class ControladorReportesEstadisticas {
-    
+
 }
