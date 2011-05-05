50b1dd104ff913fd9ff70d410b7783724fbcba16
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-04 23:24:24 -0500

    agregado metodos de DaoReportesEstadisticas

--- src/biblioteca/database2/accesoDatos/DaoReportesEstadisticas.java
+++ src/biblioteca/database2/accesoDatos/DaoReportesEstadisticas.java
@@ -8,6 +8,60 @@ package biblioteca.database2.accesoDatos;
  *
  * @author alejandro
  */
+
+import biblioteca.database2.fachada.fachada;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class DaoReportesEstadisticas {
+    fachada Fachada;
+    public void DaoReportesEstadisticas(){
+        Fachada = new fachada();
+    }
+    public ResultSet consultarDocumentosPorArea(String area){
+        return 1;
+    }
+    public ResultSet consultarUsuariosPorTipo(String tipo){
+        return 1;
+    }
     
+    public ResultSet consultarUsuariosRegistradosPorMes(int mes){
+        return 1;
+    }
+    public ResultSet consultarDocumentosMasConsultados(){
+        return 1;
+    }
+    public ResultSet consultarDocumentosPorTipoUsuario(String tipo){
+        return 1;
+    }
+    public ResultSet consultarDocumentosPorArea(ArrayList<String> areas){
+        return 1;
+    }
+    public ResultSet consultarDocumentosPorFormato(String formato){
+        return 1;
+    }
+    public ResultSet consultarDocumentoCatalogadoPorMes(int mes){
+        return 1;
+    }
+    public ResultSet consultarDocumentoCatalogadoPorArea(String area){
+        return 1;
+    }
+    public ResultSet consultarCatalogadoPorMesyArea(int mes, String area){
+        return 1;
+    }
+    public ResultSet consultarDocumentosMasDescargados(){
+        return 1;
+    }
+    public ResultSet consultarDocumentosMasDescargadosPorTipoUsuario(String tipo){
+        return 1;
+    }
+    public ResultSet consultarDocumentosMasDescargadosPorArea(ArrayList<String> areas){
+        return 1;
+    }
+    public ResultSet consultarUsuariosMayorAreaDeInteres(){
+        return 1;
+    }
+    public ResultSet consultarFranjasHorariasMasUsuarios(int franja){
+        return 1;
+    }
 }
