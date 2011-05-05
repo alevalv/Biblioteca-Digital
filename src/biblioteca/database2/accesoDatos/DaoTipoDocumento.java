b8b715700e69cd3454d1d4bf9be400c653530a37
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-05 00:29:30 -0500

    cambios menores en DaoTipoDocumento, agregada función de consultarTipoDocumento

--- src/biblioteca/database2/accesoDatos/DaoTipoDocumento.java
+++ src/biblioteca/database2/accesoDatos/DaoTipoDocumento.java
@@ -21,7 +21,7 @@ public class DaoTipoDocumento {
         fachada = new fachada();
     }
 
-    public int agregarTipo(TipoDocumento tipoDocumento)
+    public int agregarTipoDocumento(TipoDocumento tipoDocumento)
     {
        String sql_agregar;
        sql_agregar = "INSERT INTO tipo_material(tipo_documento, descripcion) VALUES ('" +
@@ -40,10 +40,9 @@ public class DaoTipoDocumento {
        return -1;
     }
 
-    public void modificarTipoDocumento(TipoDocumento tipoDocumento)
-    {
+    public int modificarTipoDocumento(TipoDocumento tipoDocumento){
         String sql_modificar;
-        sql_modificar="UPDATE programas SET tipo_documento = '"+ 
+        sql_modificar="UPDATE tipo_material SET tipo_documento = '"+ 
                 tipoDocumento.getTipoDocumento() + "' WHERE descripcion = '"
                 + tipoDocumento.getDescripcion() + "';";
         try{
@@ -51,9 +50,34 @@ public class DaoTipoDocumento {
             Statement sentencia = conn.createStatement();
             int numFilas = sentencia.executeUpdate(sql_modificar);
             conn.close();
+            return numFilas;
 
         }
         catch(SQLException e){ System.out.println(e); }
         catch(Exception e){ System.out.println(e); }
+        return -1;
+    }
+    
+    public TipoDocumento consultarDocumento(String nombre){
+        String sql_consultar;
+        sql_consultar="SELECT FROM  tipo_material WHERE tipo_documento = '" +
+                nombre + "';";
+        try{
+            Connection conn= fachada.conectar();
+            Statement sentencia = conn.createStatement();
+            ResultSet tabla = sentencia.executeQuery(sql_consultar);
+            
+            TipoDocumento tipoDocumento=null;
+            if(!tabla.wasNull()){
+                tipoDocumento.setTipoDocumento(tabla.getString(1));
+                tipoDocumento.setDescripcion(tabla.getString(2));
+            }
+            conn.close();
+            return tipoDocumento;
+
+        }
+        catch(SQLException e){ System.out.println(e); }
+        catch(Exception e){ System.out.println(e); }
+        return null;
     }
 }
