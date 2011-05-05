65e71b0024a8ab4b8a79eff2699cfa808b49e4a0
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-04 21:11:35 -0500

    agregados metodos y atributos a DaoTipoDocumento y TipoDocumento (hecho por Julián)

--- src/biblioteca/database2/accesoDatos/DaoTipoDocumento.java
+++ src/biblioteca/database2/accesoDatos/DaoTipoDocumento.java
@@ -6,8 +6,52 @@ package biblioteca.database2.accesoDatos;
 
 /**
  *
- * @author alejandro
+ * @author Julián David Rodríguez
  */
+
+import java.sql.*;
+import biblioteca.database2.beans.TipoDocumento;
+import biblioteca.database2.fachada.fachada;
+
 public class DaoTipoDocumento {
-    
+    fachada fachada;
+
+    public DaoTipoDocumento()
+    {
+        fachada = new fachada();
+    }
+
+    public int agregarTipo(TipoDocumento tipoMaterial)
+    {
+       String sql_agregar;
+       sql_agregar = "INSERT INTO tipo_material(tipo_documento, descripcion) VALUES ('" +
+               tipoMaterial.getTipoDocumento() + "','" + tipoMaterial.getDescripcion() + "')";
+
+       try{
+            Connection conn= fachada.conectar();
+            Statement sentencia = conn.createStatement();
+            int numFilas = sentencia.executeUpdate(sql_agregar);
+            conn.close();
+            return numFilas;
+        }
+        catch(SQLException e){ System.out.println(e); }
+        catch(Exception e){ System.out.println(e); }
+
+       return -1;
+    }
+
+    public void modificarTipoCodumento(String tipoDocumento, String descripcion)
+    {
+        String sql_modificar;
+        sql_modificar="UPDATE programas SET tipo_documento = '"+ tipoDocumento + "' WHERE descripcion = '"+ descripcion + "';";
+        try{
+            Connection conn= fachada.conectar();
+            Statement sentencia = conn.createStatement();
+            int numFilas = sentencia.executeUpdate(sql_modificar);
+            conn.close();
+
+        }
+        catch(SQLException e){ System.out.println(e); }
+        catch(Exception e){ System.out.println(e); }
+    }
 }
--- src/biblioteca/database2/beans/TipoDocumento.java
+++ src/biblioteca/database2/beans/TipoDocumento.java
@@ -6,8 +6,32 @@ package biblioteca.database2.beans;
 
 /**
  *
- * @author alejandro
+ * @author Julián David Rodríguez
  */
 public class TipoDocumento {
-    
+        String tipoDocumento;
+        String descripcion;
+
+        public TipoDocumento(){}
+
+        public void setTipoDocumento(String tipoDocumento)
+        {
+            this.tipoDocumento = tipoDocumento;
+        }
+
+        public void setDescripcion(String descripcion)
+        {
+            this.descripcion = descripcion;
+        }
+        
+        public String getTipoDocumento()
+        {
+            return tipoDocumento;
+        }
+
+        public String getDescripcion()
+        {
+            return descripcion;
+        }
+
 }
