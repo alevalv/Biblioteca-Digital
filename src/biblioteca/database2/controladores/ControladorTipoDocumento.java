92b95eef53e7b1b23f13d7ad8ad3dc3d6a2eb0c4
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-05 00:33:20 -0500

    agregadas funciones de agregar, modificar y consultar TipoDocumento al Controlador de TipoDocumento

--- src/biblioteca/database2/controladores/ControladorTipoDocumento.java
+++ src/biblioteca/database2/controladores/ControladorTipoDocumento.java
@@ -4,10 +4,40 @@
  */
 package biblioteca.database2.controladores;
 
+import biblioteca.database2.accesoDatos.DaoTipoDocumento;
+import biblioteca.database2.beans.TipoDocumento;
+
 /**
  *
  * @author alejandro
  */
 public class ControladorTipoDocumento {
+    public void agregarTipoDocumento(String nombre, String descripcion){
+        TipoDocumento tipoDocumento = new TipoDocumento();
+        tipoDocumento.setTipoDocumento(nombre);
+        tipoDocumento.setDescripcion(descripcion);
+        DaoTipoDocumento daoTipoDocumento = new DaoTipoDocumento();
+        daoTipoDocumento.agregarTipoDocumento(tipoDocumento);
+        
+        //seguridad?
+        tipoDocumento=null;
+        daoTipoDocumento=null;
+    }
+    public void modificarTipoDocumento(String nombre, String descripcion){
+        TipoDocumento tipoDocumento = new TipoDocumento();
+        tipoDocumento.setTipoDocumento(nombre);
+        tipoDocumento.setDescripcion(descripcion);
+        DaoTipoDocumento daoTipoDocumento = new DaoTipoDocumento();
+        daoTipoDocumento.modificarTipoDocumento(tipoDocumento);
+        
+        //seguridad?
+        tipoDocumento=null;
+        daoTipoDocumento=null;
+    }
     
+    public TipoDocumento consultarTipoDocumento(String nombre){
+        DaoTipoDocumento daoTipoDocumento = new DaoTipoDocumento();
+        TipoDocumento salida=daoTipoDocumento.consultarDocumento(nombre);
+        return salida;
+    }
 }
