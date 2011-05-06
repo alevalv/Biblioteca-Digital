f0a26147f5428e5e5072d8b6ca0aa17e309894f8
guardo823 <guardo823@gmail.com>
2011-05-06 00:03:30 -0700

    Edited src/biblioteca/database1/controladores/ControladorAutor.java via GitHub

--- src/biblioteca/database1/controladores/ControladorAutor.java
+++ src/biblioteca/database1/controladores/ControladorAutor.java
@@ -4,10 +4,62 @@
  */
 package biblioteca.database1.controladores;
 
+package controlador;
+
+import accesoDatos.*;
+import beans.*;
+
 /**
  *
- * @author alejandro
+ * @author Eduardo
  */
 public class ControladorAutor {
-    
+
+      public void insertarAutor(String nombre, String apellido, String correo_e, String acronimo)
+    {
+        DaoAutor daoAutor= new DaoAutor();
+        Autor autor= new Autor();
+
+        autor.setAcronimo(acronimo);
+        autor.setApellido(apellido);
+        autor.setCorreo(correo_e);
+        autor.setNombre(nombre);
+
+        daoAutor.guardarAutor(autor);
+    }
+
+//      public void eliminarAutor(String correo_e)
+//    {
+//        DaoAutor daoAutor= new DaoAutor();
+//        daoAutor.eliminarAutor(correo_e);
+//    }
+
+       public void modificarAutor(String correo_e, String nombre, String acronimo, String apellido)
+    {
+        DaoAutor daoAutor= new DaoAutor();
+        Autor autor= new Autor();
+
+        autor.setAcronimo(acronimo);
+        autor.setApellido(apellido);
+        autor.setCorreo(correo_e);
+        autor.setNombre(nombre);
+
+        daoAutor.modificarAutor(autor);
+    }
+
+        public Autor consultarAutor(String correo_e)
+    {
+        DaoAutor daoAutor= new DaoAutor();
+        Autor autor= new Autor();
+        autor=daoAutor.consultarAutor(correo_e);
+        return autor;
+    }
+
+          public boolean comprobarExistenciaAutor(String correo) {
+         boolean respuesta;
+         DaoAutor daoArea= new DaoAutor();
+         respuesta= daoArea.comprobarExistenciaAutor(correo);
+         return respuesta;
+    }
+
 }
