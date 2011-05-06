a42376873c5536bb0c3316e56e022d3101c5eecd
guardo823 <guardo823@gmail.com>
2011-05-05 23:56:29 -0700

    Edited src/biblioteca/database1/beans/PalabraClave.java via GitHub

--- src/biblioteca/database1/beans/PalabraClave.java
+++ src/biblioteca/database1/beans/PalabraClave.java
@@ -4,10 +4,35 @@
  */
 package biblioteca.database1.beans;
 
+package beans;
+
 /**
  *
- * @author alejandro
+ * @author Eduardo
  */
 public class PalabraClave {
-    
+
+    String nombre;
+    String descripcion;
+
+    public PalabraClave(){}
+
+     public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+
+
 }
