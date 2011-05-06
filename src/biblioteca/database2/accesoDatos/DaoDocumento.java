8c39580a352f2054c706fc5d7f4092c7654688ad
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-05 20:31:02 -0500

    agregadas funciones a varios metodos de DaoDocumento

--- src/biblioteca/database2/accesoDatos/DaoDocumento.java
+++ src/biblioteca/database2/accesoDatos/DaoDocumento.java
@@ -6,7 +6,10 @@ package biblioteca.database2.accesoDatos;
 
 import biblioteca.database2.fachada.fachada;
 import biblioteca.database2.beans.Documento;
+import java.sql.Statement;
+import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 /**
  *
@@ -18,12 +21,48 @@ public class DaoDocumento {
     Fachada = new fachada();
     }
     
-    public void insertarDocumento(Documento documento, String usuario){
-        //TODO insertarDocumento
+    public int insertarDocumento(Documento documento, String usuario){
+        String sql_agregar;
+        sql_agregar = "INSERT INTO documentos(activo, titulo_principal, "+
+                "titulo_secundario, descripcion, idioma, editorial, fecha_publicacion, " +
+                "derechos_autor, ubicacion, tipo_documento, catalogador)"+
+                "VALUES ('" + "true" + "','" + documento.getTituloPrincipal() + "','" 
+                + documento.getTituloSecundario() + "','"+ documento.getDescripcion() + "','"
+                + documento.getIdioma() + "','" + documento.getEditorial() + "','" 
+                + documento.getFechaPublicacion() + "','" + documento.getDerechosAutor() + "','"
+                + documento.getUbicacion() + "','" + documento.getTipoMaterial() + "','"
+                + usuario + "')";
+
+       try{
+            Connection conn= Fachada.conectar();
+            java.sql.Statement sentencia = conn.createStatement();
+            int num_filas=sentencia.executeUpdate(sql_agregar);
+            conn.close();
+            return num_filas;
+        }
+        catch(SQLException e){ System.out.println(e); }
+        catch(Exception e){ System.out.println(e); }
+
+       return -1;
     }
-    public void deshabilitarDocumento(String id_documento){
-        //TODO deshabilitarDocumento
+    
+    public int deshabilitarDocumento(String id_documento){
+        String sql_modificar;
+        sql_modificar="UPDATE documento SET activo = 'false' WHERE doc_id = '"
+                + id_documento + "';";
+        try{
+            Connection conn= Fachada.conectar();
+            Statement sentencia = conn.createStatement();
+            int num_filas = sentencia.executeUpdate(sql_modificar);
+            conn.close();
+            return num_filas;
+
+        }
+        catch(SQLException e){ System.out.println(e); }
+        catch(Exception e){ System.out.println(e); }
+        return -1;
     }
+    
     public int modificarDocumento(Documento documento){
         //TODO modificarDocumento
         return -1;
