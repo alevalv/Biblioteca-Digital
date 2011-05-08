334baa8bec4b4b8be3260cfb8d502dd04c3ecb79
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-08 17:50:13 -0500

    configurado para cumplir con la database2

--- src/biblioteca/database2/controladores/ControladorPalabraClave.java
+++ src/biblioteca/database2/controladores/ControladorPalabraClave.java
@@ -4,10 +4,54 @@
  */
 package biblioteca.database2.controladores;
 
+
+import biblioteca.database2.accesoDatos.DaoPalabraClave;
+import biblioteca.database2.beans.PalabraClave;
+
 /**
  *
- * @author alejandro
+ * @author Eduardo
  */
 public class ControladorPalabraClave {
-    
+
+     public void InsertarPalabraClave(String nombre, String descripcion)
+    {
+        DaoPalabraClave daoPalabraClave = new DaoPalabraClave();
+        PalabraClave palabra= new PalabraClave();
+        palabra.setNombre(nombre);
+        palabra.setDescripcion(descripcion);
+
+        daoPalabraClave.guardarPalabraClave(palabra);
+    }
+
+    public void modificarPalabra(String nombre, String descripcion)
+    {
+        DaoPalabraClave daoPalabraClave = new DaoPalabraClave();
+        PalabraClave palabra= new PalabraClave();
+        palabra.setNombre(nombre);
+        palabra.setDescripcion(descripcion);
+
+        daoPalabraClave.modificarPalabraClave(palabra);
+    }
+
+    public PalabraClave consultarPalabra(String nombre)
+    {
+        DaoPalabraClave daoPalabraClave = new DaoPalabraClave();
+        PalabraClave palabra= new PalabraClave();
+        palabra = daoPalabraClave.consultarPalabraClave(nombre);
+        return palabra;
+    }
+
+    public void eliminarPalabraClave(String nombre)
+    {
+        DaoPalabraClave daoPalabraClave = new DaoPalabraClave();
+        daoPalabraClave.eliminarPalabraClave(nombre);
+    }
+
+      public boolean comprobarExistenciaPalabraClave(String nombre) {
+         boolean respuesta;
+         DaoPalabraClave daoPalabraClave= new DaoPalabraClave();
+         respuesta= daoPalabraClave.comprobarExistenciaPalabraClave(nombre);
+         return respuesta;
+    }
 }
