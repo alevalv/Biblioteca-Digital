f6bea9be37479795dc3ad854c6310f2062de2fbc
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-08 16:42:02 -0500

    agregada funcionalidad a Tipo_Documento

--- src/biblioteca/gui/catalogacion/Tipo_Documento.java
+++ src/biblioteca/gui/catalogacion/Tipo_Documento.java
@@ -10,7 +10,12 @@
  */
 package biblioteca.gui.catalogacion;
 
+import biblioteca.database2.accesoDatos.DaoTipoDocumento;
 import biblioteca.database2.beans.Documento;
+import biblioteca.database2.beans.TipoDocumento;
+import biblioteca.database2.controladores.ControladorTipoDocumento;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
 
 /**
@@ -19,12 +24,30 @@ import javax.swing.JTabbedPane;
  */
 public class Tipo_Documento extends javax.swing.JPanel {
     Documento documento;
+    ArrayList<TipoDocumento> TiposExistentes;
+    static public TipoDocumento tipoSeleccionado;
     /** Creates new form Tipo_Documento */
     public Tipo_Documento(Documento documento) {
         initComponents();
         this.documento=documento;
+        TiposExistentes = new ArrayList<TipoDocumento>();
+        tipoSeleccionado=null;
+        initComboBox();
+        biblioteca.gui.GUICatalogacion.Tipo_Documento_Guardado=false;
     }
 
+    private void initComboBox(){
+        Tipos_Documento.removeAllItems();
+        TiposExistentes.clear();
+        TiposExistentes=new DaoTipoDocumento().consultarTodosLosTipoDocumento();
+        for(int i=0;i<TiposExistentes.size();i++){
+            Tipos_Documento.insertItemAt(TiposExistentes.get(i).getTipoDocumento(), i);
+        }
+        Tipos_Documento.setSelectedIndex(-1);
+        biblioteca.gui.GUICatalogacion.Tipo_Documento_Guardado=false;
+    }
+    
+    
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -45,20 +68,20 @@ public class Tipo_Documento extends javax.swing.JPanel {
         jLabel11 = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        Tipo_Nombre = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        Tipo_Descripcion = new javax.swing.JTextArea();
         Agregar = new javax.swing.JButton();
         Estado = new javax.swing.JLabel();
         Siguiente = new javax.swing.JButton();
 
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/biblioteca/gui/resources/logo.png"))); // NOI18N
 
-        jLabel4.setFont(new java.awt.Font("Ubuntu", 1, 24)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Ubuntu", 1, 24));
         jLabel4.setText("Tipo de Documento");
 
-        jLabel10.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
+        jLabel10.setFont(new java.awt.Font("Ubuntu", 0, 18));
         jLabel10.setText("Descripción:");
 
         Obtener_Descripcion.setText("Obtener Descripción");
@@ -75,19 +98,19 @@ public class Tipo_Documento extends javax.swing.JPanel {
         jSeparator1.setMinimumSize(new java.awt.Dimension(150, 6));
         jSeparator1.setPreferredSize(new java.awt.Dimension(200, 10));
 
-        jLabel11.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
+        jLabel11.setFont(new java.awt.Font("Ubuntu", 0, 18));
         jLabel11.setText("Tipos de documento existentes (seleccionar uno): ");
 
-        jLabel12.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
+        jLabel12.setFont(new java.awt.Font("Ubuntu", 0, 18));
         jLabel12.setText("Agregar un nuevo Tipo de Documento: ");
 
         jLabel1.setText("Nombre: ");
 
         jLabel2.setText("Descripción: ");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane2.setViewportView(jTextArea1);
+        Tipo_Descripcion.setColumns(20);
+        Tipo_Descripcion.setRows(5);
+        jScrollPane2.setViewportView(Tipo_Descripcion);
 
         Agregar.setText("Agregar");
         Agregar.addActionListener(new java.awt.event.ActionListener() {
@@ -96,11 +119,11 @@ public class Tipo_Documento extends javax.swing.JPanel {
             }
         });
 
-        Estado.setFont(new java.awt.Font("Ubuntu", 0, 24)); // NOI18N
+        Estado.setFont(new java.awt.Font("Ubuntu", 0, 24));
         Estado.setForeground(new java.awt.Color(255, 0, 0));
         Estado.setText("[Sin Guardar]");
 
-        Siguiente.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
+        Siguiente.setFont(new java.awt.Font("Ubuntu", 1, 15));
         Siguiente.setText("Siguiente Paso");
         Siguiente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -141,7 +164,7 @@ public class Tipo_Documento extends javax.swing.JPanel {
                                 .addGap(12, 12, 12)
                                 .addComponent(jLabel1)
                                 .addGap(2, 2, 2)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 487, Short.MAX_VALUE))
+                                .addComponent(Tipo_Nombre, javax.swing.GroupLayout.DEFAULT_SIZE, 487, Short.MAX_VALUE))
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel2)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -175,9 +198,7 @@ public class Tipo_Documento extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel10)
                         .addGap(2, 2, 2))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(Obtener_Descripcion)
-                        .addGap(18, 18, 18)))
+                    .addComponent(Obtener_Descripcion))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -189,7 +210,7 @@ public class Tipo_Documento extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(Tipo_Nombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(41, 41, 41)
@@ -211,25 +232,48 @@ public class Tipo_Documento extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void Obtener_DescripcionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Obtener_DescripcionActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_Obtener_DescripcionActionPerformed
 
     private void AgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarActionPerformed
-        // TODO add your handling code here:
+        if(checkEmptyFieldsTipo()){
+            new ControladorTipoDocumento().agregarTipoDocumento(Tipo_Nombre.getText(), Tipo_Descripcion.getText());
+            JOptionPane.showMessageDialog(this, "El Tipo con nombre "+Tipo_Nombre.getText()+" ha sido agregado", "Notificaición", JOptionPane.INFORMATION_MESSAGE);
+            Tipo_Nombre.setText("");
+            Tipo_Descripcion.setText("");
+            initComboBox();
+        }
     }//GEN-LAST:event_AgregarActionPerformed
 
     private void SiguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SiguienteActionPerformed
-        biblioteca.gui.GUICatalogacion.Autores_Guardado=true;
+        tipoSeleccionado = new DaoTipoDocumento().consultarDocumento((String)Tipos_Documento.getItemAt(Tipos_Documento.getSelectedIndex()));
+        biblioteca.gui.GUICatalogacion.Tipo_Documento_Guardado=true;
+        Tipos_Documento.setEnabled(false);
         JTabbedPane parent =(JTabbedPane) this.getParent();
         parent.setSelectedIndex(5);
 }//GEN-LAST:event_SiguienteActionPerformed
 
+    private boolean checkEmptyFieldsTipo(){
+        if(Tipo_Nombre.getText()==null || Tipo_Nombre.getText().equals("")){
+            JOptionPane.showMessageDialog(this, "El campo Nombre no puede estar vacio", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        else if(Tipo_Descripcion.getText()==null || Tipo_Descripcion.getText().equals("")){
+            JOptionPane.showMessageDialog(this, "El campo Descripcion no puede estar vacio", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        else return true;
+    }
+    
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Agregar;
     private javax.swing.JTextArea Descripcion;
     private javax.swing.JLabel Estado;
     private javax.swing.JButton Obtener_Descripcion;
     private javax.swing.JButton Siguiente;
+    private javax.swing.JTextArea Tipo_Descripcion;
+    private javax.swing.JTextField Tipo_Nombre;
     private javax.swing.JComboBox Tipos_Documento;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
@@ -241,7 +285,5 @@ public class Tipo_Documento extends javax.swing.JPanel {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
