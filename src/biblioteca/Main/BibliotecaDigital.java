5726f74d41b3c8710a76b262966e1fb995b47b64
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-10 01:04:17 -0500

    Main posiblemente final

--- src/biblioteca/Main/BibliotecaDigital.java
+++ src/biblioteca/Main/BibliotecaDigital.java
@@ -8,6 +8,8 @@ package biblioteca.Main;
 
 import java.io.File;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 
 /**
@@ -27,15 +29,29 @@ public class BibliotecaDigital {
         if(!dir.exists()){
             dir.mkdir();
         }
-        int opcion=JOptionPane.showConfirmDialog(null, "Presione si para gestión, presione no para catalogar un documento");
-        if(opcion==0){
-            new biblioteca.gui.GUIRegistroUsuarios().setVisible(true);
-            new biblioteca.gui.GUIModificarUsuario().setVisible(true);
-        }
-        else if(opcion==1){
-            new biblioteca.gui.GUICatalogacion().setVisible(true);
-        }
-        else System.exit(0);
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                try {
+	    // Set System L&F
+        UIManager.setLookAndFeel(
+            UIManager.getSystemLookAndFeelClassName());
+    } 
+    catch (UnsupportedLookAndFeelException e) {
+       // handle exception
+    }
+    catch (ClassNotFoundException e) {
+       // handle exception
+    }
+    catch (InstantiationException e) {
+       // handle exception
+    }
+    catch (IllegalAccessException e) {
+       // handle exception
+    }
+                new biblioteca.gui.GUIBusqueda().setVisible(true);
+            }
+        });
         
         // TODO code application logic here
     }
