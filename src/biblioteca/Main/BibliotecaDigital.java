3dbbe710929dfa5dc01ba88198dccc15a35e6880
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-09 22:20:37 -0500

    agregado programa principal para la biblioteca

--- src/biblioteca/Main/BibliotecaDigital.java
+++ src/biblioteca/Main/BibliotecaDigital.java
@@ -4,7 +4,11 @@
  */
 package biblioteca.Main;
 
-import biblioteca.gui.GUIModificarUsuario;
+//import biblioteca.gui.GUIModificarUsuario;
+
+import java.io.File;
+import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -18,7 +22,20 @@ public class BibliotecaDigital {
      */
     public static void main(String[] args) {
         LOGGED_USER="dummyuser";
-        new GUIModificarUsuario().setVisible(true);
+        String path=System.getProperty("user.dir")+"/repository/";
+        File dir= new File(path);
+        if(!dir.exists()){
+            dir.mkdir();
+        }
+        int opcion=JOptionPane.showConfirmDialog(null, "Presione si para gestión, presione no para catalogar un documento");
+        if(opcion==0){
+            new biblioteca.gui.GUIRegistroUsuarios().setVisible(true);
+            new biblioteca.gui.GUIModificarUsuario().setVisible(true);
+        }
+        else if(opcion==1){
+            new biblioteca.gui.GUICatalogacion().setVisible(true);
+        }
+        else System.exit(0);
         
         // TODO code application logic here
     }
