ebb48b22b47213b2e2de32f2a600da4da31b9798
Cristina Bustos <mcb9216@hotmail.com>
2011-05-26 16:48:00 -0500

    principio de consultas anidadas panel usuarios

--- src/biblioteca/database2/accesoDatos/DaoReportesEstadisticas.java
+++ src/biblioteca/database2/accesoDatos/DaoReportesEstadisticas.java
@@ -33,17 +33,49 @@ public class DaoReportesEstadisticas {
     public void ConsultarListaUsuario(String dow, String dom, String month, String year, String[] franja, String[] desde, String[] Hasta, String tipo,
             String genero, String Estado, String area){
       
-      String sql_consultar="Select username, nombres, apellidos, fecha_registro from usuarios where ";
-     
+      String sql_consultar="", inter=" intersect ";
+      int cont=0;
       
       sql_consultar+=ConsultarDowUsuarios(dow);
-      sql_consultar+=ConsultarDomUsuarios(dom);
-      sql_consultar+=ConsultarMonthUsuarios(month);
-      sql_consultar+=ConsultarYearUsuarios(year);
-      sql_consultar+=ConsultarIntervaloUsuarios(desde,Hasta);
-      sql_consultar+=ConsultarTipoUsuarios(tipo);
-      sql_consultar+=ConsultarGeneroUsuarios(genero);
-      sql_consultar+=ConsultarEstadoUsuarios(Estado);
+      if(!ConsultarDomUsuarios(dom).isEmpty())  {
+          cont++;
+      sql_consultar+=ConsultarDomUsuarios(dom);}
+      
+      if(!ConsultarMonthUsuarios(month).isEmpty())  {
+          if(cont>0) sql_consultar+=inter;
+          cont++;
+      sql_consultar+=ConsultarMonthUsuarios(month);}
+      
+      if(!ConsultarYearUsuarios(year).isEmpty())  {
+          if(cont>0) sql_consultar+=inter;
+          cont++;
+      sql_consultar+=ConsultarYearUsuarios(year);}
+      
+      if(!ConsultarFranjaUsuarios(franja).isEmpty())  {
+          if(cont>0) sql_consultar+=inter;
+          cont++;
+      sql_consultar+=ConsultarFranjaUsuarios(franja);}
+      
+      if(!ConsultarIntervaloUsuarios(desde,Hasta).isEmpty())  {
+         if(cont>0)  sql_consultar+=inter;
+          cont++;
+      sql_consultar+=ConsultarIntervaloUsuarios(desde,Hasta);}
+      
+      if(!ConsultarTipoUsuarios(tipo).isEmpty())  {
+          if(cont>0) sql_consultar+=inter;
+          cont++;
+      sql_consultar+=ConsultarTipoUsuarios(tipo);}
+      
+       if(!ConsultarGeneroUsuarios(genero).isEmpty())  {
+          if(cont>0) sql_consultar+=inter;
+          cont++;
+      sql_consultar+=ConsultarGeneroUsuarios(genero);}
+       
+        if(!ConsultarGeneroUsuarios(genero).isEmpty())  {
+          if(cont>0) sql_consultar+=inter;
+          cont++;
+      sql_consultar+=ConsultarEstadoUsuarios(Estado);}
+        
       sql_consultar+=";";
       
       System.out.println(sql_consultar);
@@ -64,32 +96,32 @@ public class DaoReportesEstadisticas {
     public String ConsultarDowUsuarios(String dow){
         String consultar="";
         if(dow != null){
-            consultar="date_part('dow',fecha_registro)="+dow+" "; }
+            consultar="Select username, nombres, apellidos, fecha_registro from usuarios where date_part('dow',fecha_registro)="+dow+" "; }
         return consultar;
     }
     public String ConsultarDomUsuarios(String dom){
         String consultar="";
         if(dom != null){
-            consultar="date_part('day',fecha_registro)="+dom+" "; }
+            consultar="Select username, nombres, apellidos, fecha_registro from usuarios where date_part('day',fecha_registro)="+dom+" "; }
         return consultar;
     }
     public String ConsultarMonthUsuarios(String month){
         String consultar="";
         if(month != null){
-            consultar="date_part('month',fecha_registro)="+month+" "; }
+            consultar="Select username, nombres, apellidos, fecha_registro from usuarios where date_part('month',fecha_registro)="+month+" "; }
         return consultar;
     }
     public String ConsultarYearUsuarios(String year){
         String consultar="";
         if(year != null){
-            consultar="date_part('year',fecha_registro)="+year+" "; }
+            consultar="Select username, nombres, apellidos, fecha_registro from usuarios where date_part('year',fecha_registro)="+year+" "; }
         return consultar;
     }
      public String ConsultarTipoUsuarios(String tipo){
         String consultar="";
         if(tipo != null){
             if(tipo.equals("1"))
-            consultar="tipo_usuario='3' ";
+            consultar="Select username, nombres, apellidos, fecha_registro from usuarios where tipo_usuario='3' ";
             else  consultar="tipo_usuario='"+tipo+"' ";
         }
         return consultar;
@@ -98,8 +130,8 @@ public class DaoReportesEstadisticas {
         String consultar="";
         if(genero != null){
             if(genero.equals("1"))
-            consultar="genero='M' ";
-            else  consultar="genero='F' ";
+            consultar="Select username, nombres, apellidos, fecha_registro from usuarios where genero='M' ";
+            else  consultar="Select username, nombres, apellidos, fecha_registro from usuarios where genero='F' ";
         }
         return consultar;
     }
@@ -107,73 +139,26 @@ public class DaoReportesEstadisticas {
         String consultar="";
         if(Estado != null){
             if(Estado.equals("1"))
-            consultar="activo='t' ";
-            else  consultar="activo='f' ";
+            consultar="Select username, nombres, apellidos, fecha_registro from usuarios where activo='t' ";
+            else  consultar="Select username, nombres, apellidos, fecha_registro from usuarios where activo='f' ";
         }
         return consultar;
     }
-    /*public String ConsultarFranjaUsuarios(String[] franja){
+    public String ConsultarFranjaUsuarios(String[] franja){
         String consultar="";
         if(franja != null){
-            consultar="date_part('year',fecha_registro)="+franja+" "; }
+            consultar="Select username, nombres, apellidos, fecha_registro from usuarios where date_part('hour',fecha_registro)>="+franja[0]+" and date_part('hour',fecha_registro)<="+franja[1]+" "; }
         return consultar;
-    }*/
+    }
     public String ConsultarIntervaloUsuarios(String[] desde, String[] Hasta){
         String consultar="";
         if(desde != null && Hasta != null){
-            consultar="fecha_registro between '"+desde[0]+"-"+desde[1]+"-"+desde[2]+"' AND '"
+            consultar="Select username, nombres, apellidos, fecha_registro from usuarios where fecha_registro between '"+desde[0]+"-"+desde[1]+"-"+desde[2]+"' AND '"
                     +Hasta[0]+"-"+Hasta[1]+"-"+Hasta[2]+"' ";
         }
         return consultar;
     }
     
     
-    /*
-    public ResultSet consultarDocumentosPorArea(String area){
-        return 1;
-    }
-    public ResultSet consultarUsuariosPorTipo(String tipo){
-        return 1;
-    }
-    
-    public ResultSet consultarUsuariosRegistradosPorMes(int mes){
-        return 1;
-    }
-    public ResultSet consultarDocumentosMasConsultados(){
-        return 1;
-    }
-    public ResultSet consultarDocumentosPorTipoUsuario(String tipo){
-        return 1;
-    }
-    public ResultSet consultarDocumentosPorArea(ArrayList<String> areas){
-        return 1;
-    }
-    public ResultSet consultarDocumentosPorFormato(String formato){
-        return 1;
-    }
-    public ResultSet consultarDocumentoCatalogadoPorMes(int mes){
-        return 1;
-    }
-    public ResultSet consultarDocumentoCatalogadoPorArea(String area){
-        return 1;
-    }
-    public ResultSet consultarCatalogadoPorMesyArea(int mes, String area){
-        return 1;
-    }
-    public ResultSet consultarDocumentosMasDescargados(){
-        return 1;
-    }
-    public ResultSet consultarDocumentosMasDescargadosPorTipoUsuario(String tipo){
-        return 1;
-    }
-    public ResultSet consultarDocumentosMasDescargadosPorArea(ArrayList<String> areas){
-        return 1;
-    }
-    public ResultSet consultarUsuariosMayorAreaDeInteres(){
-        return 1;
-    }
-    public ResultSet consultarFranjasHorariasMasUsuarios(int franja){
-        return 1;
-    }
-    */
+   
 }
