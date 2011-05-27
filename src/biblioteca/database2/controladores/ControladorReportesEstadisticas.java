98fe0864f7851cec44c3f5c89a4714d37b2a2ad1
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-26 19:20:05 -0500

    agregados metodo para convertir de ResultSet a PdfPTable (iText)

--- src/biblioteca/database2/controladores/ControladorReportesEstadisticas.java
+++ src/biblioteca/database2/controladores/ControladorReportesEstadisticas.java
@@ -5,27 +5,42 @@
 package biblioteca.database2.controladores;
 
 import biblioteca.database2.accesoDatos.DaoReportesEstadisticas;
-import biblioteca.database2.beans.Area;
+import com.itextpdf.text.pdf.PdfPTable;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 /**
  *
  * @author alejandro
  */
 
-/*
- * Leyendo el diagrama de clases de los reportes, parece que no hay mucho cambio entre
- * el controlador y la clase DAO, tienen los mismos metodos con entradas similares y
- * salidas similares, solo con nombres diferentes, creo que esta clase debe ser eliminada
- * y usar directamente el DAO.
- */
 public class ControladorReportesEstadisticas {
 
+    private PdfPTable resultSetToTable(ResultSet entrada){
+        PdfPTable salida = null;
+        try{
+            ResultSetMetaData rsmd = entrada.getMetaData();
+        
+            salida = new PdfPTable(rsmd.getColumnCount());
+            for(int i=1;i<=rsmd.getColumnCount();i++){
+                salida.addCell(rsmd.getColumnName(i));
+            }
+            while(entrada.next()){
+                for(int i=1;i<=rsmd.getColumnCount();i++){
+                    salida.addCell(entrada.getString(i));
+                }
+            }
+        }catch(java.sql.SQLException sqlex){
+            System.err.println(sqlex);
+        }
+        return salida;
+    }
     
-   public void ConsultarListaUsuario(String dow, String dom, String month, String year, String[] franja, String[] desde, String[] Hasta, String tipo,
+    
+   public PdfPTable ConsultarListaUsuario(String dow, String dom, String month, String year, String[] franja, String[] desde, String[] Hasta, String tipo,
             String genero, String Estado, String area){
        ///Esto debe retornar una result set!!
-       ResultSet salida= new DaoReportesEstadisticas().ConsultarListaUsuario(dow, dom, month, year, franja, desde, Hasta, tipo, genero, Estado, area);
+       return resultSetToTable(new DaoReportesEstadisticas().ConsultarListaUsuario(dow, dom, month, year, franja, desde, Hasta, tipo, genero, Estado, area));
     }
 
     public void ConsultarListaDocumentosExistentes(String area, String autor, String tipo, String editorial, String idioma, String estado, String[] desde, String[] hasta) {
