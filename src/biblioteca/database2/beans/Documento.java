a7b256c1f7a8691744a5cd577a01b9a313f764fd
Alejandro Valdés <alejandrovaldes@live.com>
2011-05-31 20:34:40 -0500

    agregados comentarios al bean de Documento

--- src/biblioteca/database2/beans/Documento.java
+++ src/biblioteca/database2/beans/Documento.java
@@ -51,96 +51,194 @@ public class Documento {
 
      public void setID_documento(String id_documento) {
         this.id_documento = id_documento;
-    }
+     }
      
+     /**
+      * Cambia el estado del documento seleccionado
+      * @param activo boolean con el valor para el campo activo
+      */
      public void setActivo(boolean activo){
          this.activo=activo;
-    }
+     }
 
-      public void setEditorial(String editorial) {
+     /**
+      * Cambia la editorial del documento seleccionado
+      * @param editorial String con la editorial
+      */
+     public void setEditorial(String editorial) {
         this.editorial = editorial;
-    }
+     }
 
+     /**
+      * Cambia los derechos de Autor del documento seleccionado
+      * @param derechosAutor String con el derecho de autor para el documento
+      */
       public void setDerechosAutor(String derechosAutor) {
         this.derechosAutor = derechosAutor;
-    }
+      }
 
+     /**
+       * Cambia el idioma del docuemnto seleccionado
+       * @param idioma el String con el idioma del documento
+       */ 
      public void setIdioma(String idioma) {
         this.idioma = idioma;
     }
 
+     /**
+      * Cambia la descripción del documento seleccionado
+      * @param descripcion el String con la nueva descripción para el documento
+      */
      public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
 
+     /**
+      * Cambia el titulo principal del documento seleccionado
+      * @param tituloPrincipal el String con el titulo principal del documento
+      */
      public void setTituloPrincipal(String tituloPrincipal) {
         this.tituloPrincipal = tituloPrincipal;
     }
 
+     /**
+      * Cambia el titulo secundario del documento seleccionado
+      * @param tituloSecundario el String con el titulo secundario del documento
+      */
      public void setTituloSecundario(String tituloSecundario) {
         this.tituloSecundario = tituloSecundario;
     }
 
+     /**
+      * Cambia la fecha de publicación del documento seleccionado
+      * @param fechaPublicacion el String con la fecha de publicación, la fecha debe estar en un formato aceptado por SQL
+      */
      public void setFechaPublicacion(String fechaPublicacion) {
         this.fechaPublicacion = fechaPublicacion;
     }
 
+     /**
+      * Cambia la fecha de catalogacion del documento seleccionado
+      * es recomendable no modificar esta fecha debido a que es generada
+      * automaticamente por la base de datos
+      * @param fechaCatalogacion el String con la fecha de catalogación, la fecha debe estar en un formato aceptado por SQL
+      */
      public void setFechaCatalogacion(String fechaCatalogacion) {
         this.fechaCatalogacion = fechaCatalogacion;
     }
      
+     /**
+      * cambia el tipo de material del documento, este tipo de material
+      * debe existir en la tabla tipo_material
+      * @param tipoMaterial el String que representa el tipo de material
+      */
      public void setTipoMaterial(String tipoMaterial) {
         this.tipoDocumento = tipoMaterial;
     }
      
+     /**
+      * Cambia la ubicación del documento (el path) que se guarda en la base de datos
+      * @param ubicacion el String representando el path del documento seleccionado
+      */
      public void setUbicacion(String ubicacion) {
          this.ubicacion=ubicacion;
      }
 
+     /**
+      * Obtiene el identificador unico del documento
+      * @return el String que representa el identificador numerico del documento seleccionado
+      */
      public String getID_documento() {
         return id_documento;
      }
      
+     /**
+      * Obtiene el valor de Activo del documento seleccionado
+      * @return boolean representando el valor de Activo del documento
+      */
      public boolean getActivo() {
          return activo;
      }
 
+     /**
+      * Obtiene la descripción del documento seleccionado
+      * @return el String que contiene la descripción del documento
+      */
      public String getDescripcion() {
         return descripcion;
      }
 
+     /**
+      * Obtiene los derechos de Autor del documento seleccionado
+      * @return el String que contiene los derechos de Autor del documento seleccionado
+      */
      public String getDerechosAutor() {
         return derechosAutor;
      }
 
+     /**
+      * Obtiene la editorial del documento seleccionado
+      * @return el String que contiene la editorial del documento seleccionado
+      */
      public String getEditorial() {
         return editorial;
      }
 
+     /**
+      * Obtiene el idioma del documento seleccionado
+      * @return el String que contiene el idioma del documento seleccionado
+      */
      public String getIdioma() {
         return idioma;
      }
 
+     /**
+      * Obtiene el titulo principal del documento seleccionado
+      * @return el String que contiene el titulo principal del documento seleccionado
+      */
      public String getTituloPrincipal() {
         return tituloPrincipal;
      }
 
+     /**
+      * Obtiene el titulo secundario del documento seleccionado
+      * @return el String que contiene el titulo secundario del documento seleccionado
+      */
      public String getTituloSecundario() {
         return tituloSecundario;
      }
 
+     /**
+      * Obtiene la fecha de publicación del documento seleccionado,
+      * esta fecha esta en el formato YYYY-MM-DD
+      * @return el String que contiene la fecha de publicación del documento seleccionado
+      */
      public String getFechaPublicacion() {
         return fechaPublicacion;
      }
 
+     /**
+      * Obtiene la fecha de catalogación del documento seleccionado,
+      * esta fecha esta en el formato YYYY-MM-DD
+      * @return el String que contiene la fecha de catalogación del documento seleccionado
+      */
      public String getFechaCatalogacion() {
         return fechaCatalogacion;
      }
 
+     /**
+      * Obtiene el tipo de material del documento seleccionado,
+      * este, es una <b>foreign key</b> de la tabla tipo_material
+      * @return el String que contiene el tipo de material relacionado con el documento seleccionado
+      */
      public String getTipoMaterial() {
         return tipoDocumento;
      }
      
+     /**
+      * Obtiene la ubicación del documento seleccionado
+      * @return el String que contiene la ubicación del documento seleccionado
+      */
      public String getUbicacion() {
          return ubicacion;
      }
