481dc949e492b1e28ba15319d376b025ab236912
Alejandro Valdés <alejandrovaldes@live.com>
2011-06-04 21:56:24 -0500

    agregado manejo de los comboBox de descargas

--- src/biblioteca/database2/accesoDatos/DaoEstadisticas.java
+++ src/biblioteca/database2/accesoDatos/DaoEstadisticas.java
@@ -24,12 +24,24 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
+ *  Esta clase forma parte de los controladores creados para cumplir con el Patrón
+ * de diseño DAO.
+ * 
+ * <br>DaoEstadisticas crea el sql que se ejecutará a través de una conexión de una Fachada,
+ * este dao tiene que ver con todo lo relacionado con la generación de estadisticas sobre
+ * la base de datos
+ * 
+ * @see <a href="http://www.proactiva-calidad.com/java/patrones/DAO.html">Patrón "Data Access Object"</a>
  *
- * @author alejandro
+ * @author María Cristina Bustos Rodríguez
+ * @author Alejandro Valdés Villada
  */
 public class DaoEstadisticas {
     Fachada Fachada;
 
+    /**
+     * Crea un objeto DaoEstadisticas, inicializando la fachada
+     */
     public DaoEstadisticas() {
         Fachada=new Fachada();
     }
@@ -43,9 +55,7 @@ public class DaoEstadisticas {
                     + "documentos JOIN usuarios ON usuario_descarga_documento.username=usuarios.username ";
             if(franja!=null){
                 sql_descargados+="WHERE date_part('hour',fecha_hora)>="+franja[0]+" and date_part('hour',fecha_hora)<="+franja[1]+" ";
-            }
-            sql_descargados+="GROUP BY Identificacion, Titulo ORDER BY  descargas DESC; ";
-                    
+            }       
         }
         else{
             sql_descargados="SELECT doc_id as Identificacion, documentos."
@@ -82,8 +92,10 @@ public class DaoEstadisticas {
             for(int i=0;i<condiciones.size();i++){
                 sql_descargados+=(i!=(condiciones.size()-1)) ? condiciones.get(i)+"AND " : condiciones.get(i);
             }
-            sql_descargados+="GROUP BY Identificacion, Titulo ORDER BY  descargas DESC; ";
         }
+        
+        sql_descargados+=crearCondicionesEspecialesDescargados(tipo_usuario, area, autor, doc_tipo, usuario);
+        sql_descargados+=" GROUP BY Identificacion, Titulo ORDER BY  descargas DESC; ";
         System.out.println(sql_descargados);
         try {
             Connection conn = Fachada.conectar();
@@ -100,6 +112,43 @@ public class DaoEstadisticas {
         }
         return null;
        
+    }
+    
+    private String crearCondicionesEspecialesDescargados(String tipo_usuario, String area,String autor,String doc_tipo,String usuario){
+        String salida="";
+        ArrayList condiciones = new ArrayList(5);
+        if(tipo_usuario!=null){
+            String temp;
+            temp="(SELECT DISTINCT doc_id FROM usuario_descarga"
+                    + "_documento NATURAL JOIN usuarios WHERE tipo_usuario="+
+                    ((tipo_usuario.equals("Usuario Normal")) ? "'3'" : "'2'") +
+                    ((usuario!=null) ? " AND username='"+usuario : "")+")";
+            condiciones.add(temp);
+        }
+        if(area!=null){
+            String temp;
+            temp="(SELECT DISTINCT doc_id FROM documento_areas"
+                    + "_computacion WHERE area_id='" + area +"')";
+            condiciones.add(temp);
+        }
+        if(autor!=null){
+            String temp;
+            temp="(SELECT DISTINCT doc_id FROM documento_autor"
+                    +" WHERE autor_correo='"+autor+"')";
+            condiciones.add(temp);
+        }
+        if(doc_tipo!=null){
+            String temp;
+            temp="(SELECT DISTINCT doc_id FROM documentos WHERE "
+                    +"tipo_documento='"+doc_tipo+"')";
+            condiciones.add(temp);
+        }
+        if(!condiciones.isEmpty())
+            salida+=" AND doc_id IN(";
+        for(int i=0;i<condiciones.size();i++){
+            salida+=(i!=(condiciones.size()-1)) ? (condiciones.get(i) + " INTERSECT ") : condiciones.get(i) + ")";
+        }
+        return salida;
     }
     
     public ResultSet documentosMasConsultados(String dow,String dom,String month,String year,String tipo_usuario,
